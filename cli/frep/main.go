96f77b7c12f6594b084604eddc9cc151632ee965
Tomislav Rigo <tomislav.rigo@gmail.com>
2026-03-02 15:20:46 +0100

    Create cli entry point

--- cli/frep/main.go
+++ cli/frep/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/TomislavRigo/frep/internal/scanner"
+)
 
 func main() {
-	fmt.Println("Hello")
+	pattern := os.Args[1]
+	rawPath := os.Args[2]
+
+	for _, arg := range os.Args {
+		fmt.Println(arg)
+	}
+
+	path, err := filepath.Abs(filepath.Clean(rawPath))
+	if err != nil {
+		panic(err)
+	}
+
+	scanner.ScanAndMatch(path, pattern)
 }
